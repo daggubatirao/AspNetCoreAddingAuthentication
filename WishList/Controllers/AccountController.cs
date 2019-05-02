ccd15f22a360f28868ce42452f6c70e1f9405a9a
daggubatirao <daggubati.rao@gmail.com>
2019-05-02 09:47:21 -0700

    Add files via upload

--- WishList/Controllers/AccountController.cs
+++ WishList/Controllers/AccountController.cs
@@ -1,1 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WishList.Models;
 
+namespace WishList.Controllers
+{
+    [Authorize]
+    public class AccountController : Controller
+    {
+        private UserManager<ApplicationUser> _userManager;
+        private SignInManager<ApplicationUser> _signInManager;
+
+        public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
+        {
+            this._userManager = userManager;
+            this._signInManager = signInManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
