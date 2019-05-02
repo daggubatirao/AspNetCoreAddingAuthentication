1fe83a11962c33fe29021d381dca87f3acafb9fb
daggubatirao <daggubati.rao@gmail.com>
2019-05-02 16:54:42 -0700

    Add files via upload

--- WishList/Controllers/ItemController.cs
+++ WishList/Controllers/ItemController.cs
@@ -1,21 +1,29 @@
 ﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using WishList.Data;
+using WishList.Models;
 
 namespace WishList.Controllers
 {
+    [Authorize]
     public class ItemController : Controller
     {
         private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
 
-        public ItemController(ApplicationDbContext context)
+        public ItemController(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
         {
             _context = context;
+            _userManager = userManager;
         }
 
         public IActionResult Index()
         {
-            var model = _context.Items.ToList();
+            var loggedinUser = _userManager.GetUserAsync(HttpContext.User);
+            var model = _context.Items.ToList().Where(e => e.Id == loggedinUser.Id);
 
             return View("Index", model);
         }
@@ -27,16 +35,22 @@ namespace WishList.Controllers
         }
 
         [HttpPost]
-        public IActionResult Create(Models.Item item)
-        {
+        public async Task<ActionResult> Create(Models.Item item)
+        {            
+            item.User = await _userManager.GetUserAsync(HttpContext.User); 
             _context.Items.Add(item);
             _context.SaveChanges();
             return RedirectToAction("Index");
         }
 
-        public IActionResult Delete(int id)
+        public async Task<ActionResult> Delete(int id)
         {
+            var loggedinUser = await _userManager.GetUserAsync(HttpContext.User);
             var item = _context.Items.FirstOrDefault(e => e.Id == id);
+
+            if (item.User.Id != loggedinUser.Id)
+                return Unauthorized();
+
             _context.Items.Remove(item);
             _context.SaveChanges();
             return RedirectToAction("Index");
